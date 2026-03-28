b3bd2343ffd23864d80996fd6fa8a203674e28dd
HayiderHasan18 <hayiderhasan26@gmail.com>
2026-03-28 14:55:36 +0300

    some change

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ import dashboardRoutes from './routes/dashboardRoute.js'
 import path from "path";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://customer-support-hayidar.netlify.app/"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
