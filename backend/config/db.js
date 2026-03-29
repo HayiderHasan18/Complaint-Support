6423c3ba55caf0eb40198a9bcf3d1bdffae41a52
HayiderHasan18 <hayiderhasan26@gmail.com>
2026-03-29 18:03:12 +0300

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,15 +7,22 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT, 
   connectionLimit: 10,
+
+  
+  ssl: {
+    rejectUnauthorized: true
+  }
 }).promise();
+
 (async () => {
   try {
     const conn = await db.getConnection();
     console.log("database connected successfully!");
     conn.release();
   } catch (err) {
-    console.error("database  connection failed:", err.message);
+    console.error("database connection failed:", err.message);
   }
 })();
 
